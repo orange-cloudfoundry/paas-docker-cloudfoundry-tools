c2a84bcc06551b94c766b2d0c79551b6d63688ff
Olivier ORAND <olivier.orand@orange.com>
2023-05-23 10:20:55 +0000

    chore(renovate): try to update version in tests

--- cf-cli/cf-cli_spec.rb
+++ cf-cli/cf-cli_spec.rb
@@ -5,6 +5,7 @@ require 'serverspec'
 CF_CLI_VERSION="8.6.0"
 SPRUCE_BIN = "/usr/local/bin/spruce"
 SPRUCE_VERSION = "1.30.2"
+RUBY_VERSION = "3.1"
 
 describe "cf-cli image" do
   before(:all) {
@@ -46,10 +47,10 @@ describe "cf-cli image" do
     expect(cmd.exit_status).to eq(0)
   end
 
-  it "has ruby 3.1 available" do
+  it "has ruby #{RUBY_VERSION} available" do
     cmd = command("ruby -v")
     expect(cmd.exit_status).to eq(0)
-    expect(cmd.stdout).to match(/^ruby 3.1/)
+    expect(cmd.stdout).to match(/^ruby #{RUBY_VERSION}/)
   end
 
   it "has ruby json gem available" do
