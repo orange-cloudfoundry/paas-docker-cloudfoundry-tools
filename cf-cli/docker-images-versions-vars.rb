d8204e98217f043922583e57677199b7f64e4687
Guillaume Berche <guillaume.berche@orange.com>
2023-05-25 16:50:09 +0200

    gh releases

--- cf-cli/docker-images-versions-vars.rb
+++ cf-cli/docker-images-versions-vars.rb
@@ -1,14 +1,5 @@
 ---
-boshprometheus/bosh-exporter: "v3.3.0"
-boynux/squid-exporter: "v1.9.2"
-busybox: "1.27.1"
-ruby: "3.0"
-
-envoyproxy/envoy: "v1.10.0"
+mirror/busybox: "1.27.1"
+ruby/ruby: "3.0"
 
 mindflavor/prometheus-wireguard-exporter: "3.0.0"
-niedbalski/openstack-exporter-linux-amd64: "v1.3.0"
-pryorda/vmware_exporter: "v0.18.3"
-
-sameersbn/squid: "3.5.27-2"
-thanosio/thanos: "v0.23.1"
